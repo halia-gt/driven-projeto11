3c25b02702b80b0d30e1d4d421d1bce148f11c1e
Nathalia <nathalia.gramst@gmail.com>
2022-08-26 18:24:28 -0300

    Post tweets

--- src/index.js
+++ src/index.js
@@ -6,9 +6,19 @@ app.use(cors());
 app.use(express.json());
 
 const users = [];
+const tweets = [];
 
 app.post('/sign-up', (req, res) => {
     users.push(req.body);
+    res.send('OK');
+});
+
+app.post('/tweets', (req, res) => {
+    const avatar = users.find(user => user.username === req.body.username).avatar;
+    tweets.push({
+        ...req.body,
+        avatar
+    });
     res.send('OK');
 });
 
